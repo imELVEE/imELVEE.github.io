457341352c3b4caa834fafe63eff301a02b7f270
Aziz Alvi <ommarashid@gmail.com>
2024-12-29 05:56:36 -0500

    refactor assets folder and add transcript

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,4 @@
-import about_profile from '@assets/about_profile.jpg';
+import about_profile from '@images/about_profile.jpg';
 
 import '@/App.css';
 import "./About.css";
@@ -14,16 +14,16 @@ const About = () => {
 </p>
 <p>
     My name is Alvi Aziz, and I am a soon-to-be graduate of Hunter College with a passion for software development. 
-    Over the years, I have nurtured this passion through extensive studying and hands-on learning.
-     Initially, I explored various fields, including web development, mobile development, and game development. 
-     Along the way, I also gained knowledge in database management, graph theory, and networking. 
-     This broad foundation has given me a well-rounded perspective on the world of software development.
+    Over the years, I have nurtured this passion through extensive studying and hands-on learning. 
+    Initially, I explored various fields, including web development, mobile development, and game development. 
+    Along the way, I also gained knowledge in database management, graph theory, and networking. 
+    This broad foundation has given me a well-rounded perspective on the world of software development.
 </p>
 <p>
-    My curiosity has always driven me to delve deeper into any subject matter.
-     Whether it's understanding the inner workings of a system, equation, or protocol, I strive for a comprehensive understanding. 
-     For example, I am currently exploring Azure DevOps and its applications for my next project, as well as honing my skills in React while building this portfolio. 
-     As a former STEM tutor, I enjoy the process of learning and problem-solving, which has equipped me to adapt to various work environments and challenges.
+    My curiosity has always driven me to delve deeper into any subject matter. 
+    Whether it's understanding the inner workings of a system, equation, or protocol, I strive for a comprehensive understanding. 
+    For example, I am currently exploring Azure DevOps and its applications for my next project, as well as honing my skills in React while building this portfolio. 
+    As a former STEM tutor, I enjoy the process of learning and problem-solving, which has equipped me to adapt to various work environments and challenges.
 </p>
 <p>
     Through my experiences, I have also discovered what I enjoy most in the field of computer science. 
