30134b2ccfb45fcd66ccaea6f11bcf2f39e0042b
Alvi Aziz <imELVEE55@gmail.com>
2024-12-29 20:23:02 -0500

    very basic loading of git activity

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ActivityCalendar } from 'react-activity-calendar';
 import { Tooltip as ReactTooltip } from 'react-tooltip';
 
@@ -6,27 +6,94 @@ import 'react-tooltip/dist/react-tooltip.css';
 import './Calendar.css';
 
 const Calendar = () => {
-    const data = [
+    const [data, setData] = useState([
         {
-            date: '2024-06-23',
-            count: 2,
-            level: 1,
+          date: '2024-06-23',
+          count: 2,
+          level: 1,
         },
         {
-            date: '2024-08-02',
-            count: 16,
-            level: 4,
+          date: '2024-08-02',
+          count: 16,
+          level: 4,
         },
         {
-            date: '2024-11-29',
-            count: 11,
-            level: 3,
+          date: '2024-11-29',
+          count: 11,
+          level: 3,
         },
-    ];
+      ]
+    );
 
     useEffect(() => {
+        const loadGithubActivity = async () => {
+            // 1. Call Github events API
+            const githubId = 'imELVEE';
+            const githubApi = `https://api.github.com/users/${githubId}/events`;
+
+            const queryParams = {
+                per_page: 100,
+            };
+            const queryString = new URLSearchParams(queryParams).toString();
+            const url = `${githubApi}?${queryString}`;
+
+            const headers = {
+                'Content-Type': 'application/json',
+                'X-GitHub-Api-Version': '2022-11-28'
+            };
+
+            const options = {
+                method: 'GET',
+                headers: headers,
+            };
             
+
+            const response = await fetch(url, options);
+            const payload = await response.json();
+        
+            // 2. Report error if there's
+            if (payload?.message) {
+                return;
+            }
+        
+            // 3. Construct the data array which expects obj with 2 keys
+            // date: 'yyyy-MM-dd'
+            // activities: array of anything
+            let eventObj = {};
+            payload.forEach((event) => {
+                const date = event.created_at.slice(0, 10);
+                if (!eventObj[date]) {
+                eventObj[date] = [event];
+                } else {
+                eventObj[date] = [...eventObj[date], event];
+                }
+            });
+            
+            const newData = Object.entries(eventObj).map((entry) => {
+                console.log(`${entry[0]} : ${entry[1]}`);
+                return {
+                    date: entry[0],
+                    count: entry[1].length,
+                    level: determineLevel(entry[1].length),
+                };
+            });
+            setData(newData);
+        }
+
+        loadGithubActivity()
     }, []);
+
+    const determineLevel = (count) => {
+        if (count <= 5) {
+            return 1;
+        } else if (count <= 10) {
+            return 2;
+        } else if (count <= 15) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
 
     return (
         <div className="calendar">
